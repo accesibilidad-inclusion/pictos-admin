79d3f6af422c008b6cf5b2310fd6e62cb519514e
Diego Arenas <jacodiego@hotmail.com>
2020-07-07 07:05:47 -0500

    Dashboard Reports

--- src/views/Dashboard/Index.vue
+++ src/views/Dashboard/Index.vue
@@ -22,6 +22,19 @@
       <v-col cols="6">
         <v-card>
           <v-card-title class="headline grey lighten-2" primary-title>
+            Evaluaciones
+          </v-card-title>
+          <v-card-text>
+            <div v-for="step in steps" v-bind:key="step.id">
+              <span>{{ moment(step.created_at).format('DD/MM/YYYY \- HH:mm') }}</span>
+              <span> - <router-link :to="'/tareas/'+step.id">{{ step.label }}</router-link></span>
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="6">
+        <v-card>
+          <v-card-title class="headline grey lighten-2" primary-title>
             Tareas <router-link :to="{ name: 'Tasks', params: { status: 'contributions' }}">Ver todos</router-link>
           </v-card-title>
           <v-card-text>
@@ -41,6 +54,19 @@
             <div v-for="step in steps" v-bind:key="step.id">
               <span>{{ moment(step.created_at).format('DD/MM/YYYY \- HH:mm') }}</span>
               <span> - <router-link :to="'/tareas/'+step.id">{{ step.label }}</router-link></span>
+            </div>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="12">
+        <v-card>
+          <v-card-title class="headline grey lighten-2" primary-title>
+            Informe de errores
+          </v-card-title>
+          <v-card-text>
+            <div v-for="report in reports" v-bind:key="report.id">
+              <span>{{ moment(report.created_at).format('DD/MM/YYYY \- HH:mm') }}</span>
+              <span> - {{ report.report }} en <router-link :to="'/tareas/'+report.task.id">{{ report.task.title }}</router-link></span>
             </div>
           </v-card-text>
         </v-card>
@@ -67,12 +93,16 @@ export default {
     this.$http.get(process.env.VUE_APP_API_DOMAIN + 'api/steps/by_users').then((response) => {
       this.steps = response.data;
     });
+    this.$http.get(process.env.VUE_APP_API_DOMAIN + 'api/reports').then((response) => {
+      this.reports = response.data;
+    });
   },
   data() {
     return {
       venues: [],
       tasks: [],
-      steps: []
+      steps: [],
+      reports: []
     };
   },
   methods: {
